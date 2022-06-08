01046e21c1c58ba9498b96dcd6d2b3d281eaf2d0
mfmfe <mariafernanda.mferreira@gmail.com>
2022-06-08 15:05:29 +0100

    updates to share my code

--- utils.py
+++ utils.py
@@ -11,22 +11,23 @@ cmap_bg='Pastel1'
 cmap_fg='Set1'
 
 
-def plot_2d(X, y):
+def plot_2d(X, y, title=''):
     if X.shape[1] > 2:
         X_2d = PCA(n_components=2).fit_transform(X)
     else:
         X_2d = X
     
     plt.scatter(X_2d[:, 0], X_2d[:, 1], c=y, cmap=cmap_fg)
+    plt.title(title)
     plt.xlabel('$x_1$')
     plt.ylabel('$x_2$')
     plt.show()
 
 
-def plot_classifier_boundary(model, X, y, sc=None, h=.05):
+def plot_classifier_boundary(model, X, y, sc=None, h=.05, title=''):
     assert X.shape[1] == 2, 'The dataset needs to be 2 dimentional'
     # this function can be used with any sklearn classifier
-    # ready for two classes but can be easily extended]
+    # ready for two classes but can be easily extended
     x_min, x_max = X[:, 0].min()-.2, X[:, 0].max()+.2
     y_min, y_max = X[:, 1].min()-.2, X[:, 1].max()+.2
     # generate a grid with step h
@@ -44,7 +45,7 @@ def plot_classifier_boundary(model, X, y, sc=None, h=.05):
     plt.ylim((y_min,y_max))
     
     plt.scatter(X[:,0],X[:,1],c=y,cmap=cmap_fg)
-    #plt.title('test')
+    plt.title(title)
     plt.xlabel('$x_1$')
     plt.ylabel('$x_2$')
     
